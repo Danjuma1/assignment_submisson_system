cd90c7ded113c2420a2e31e9e28b85bdb8669ec2
Akorede Habeebullah <hda0202@gmail.com>
2021-10-15 11:42:47 +0100

    worked on views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,174 @@
-from django.shortcuts import render
+from django.contrib import messages, auth
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView, FormView, RedirectView, UpdateView
+from django.http import Http404
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import *
+from .models import User
+from .decorators import user_is_student, user_is_lecturer
+
+
+
+# STUDENT REGISTRATION VIEW
+class RegisterStudentView(CreateView):
+    model = User
+    form_class = StudentRegistrationForm
+    template_name = 'accounts/student/register.html'
+    success_url = '/'
+
+    extra_context = {
+        'title': 'Register'
+    }
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return HttpResponseRedirect(self.get_success_url())
+        return super().dispatch(self.request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+
+        form = self.form_class(data=request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            password = form.cleaned_data.get("password1")
+            user.set_password(password)
+            user.save()
+            return redirect('accounts:login')
+        else:
+            return render(request, 'accounts/student/register.html', {'form': form})
+
+
+# STUDENT PROFILE EDIT VIEW
+class EditStudentProfileView(UpdateView):
+    model = User
+    form_class = StudentProfileUpdateForm
+    context_object_name = 'student'
+    template_name = 'accounts/student/edit-profile.html'
+    success_url = reverse_lazy('accounts:student-profile-update')
+
+    @method_decorator(login_required(login_url=reverse_lazy('accounts:login')))
+    @method_decorator(user_is_student)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(self.request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        try:
+            self.object = self.get_object()
+        except Http404:
+            raise Http404("User doesn't exists")
+        # context = self.get_context_data(object=self.object)
+        return self.render_to_response(self.get_context_data())
+
+    def get_object(self, queryset=None):
+        obj = self.request.user
+        print(obj)
+        if obj is None:
+            raise Http404("Student doesn't exists")
+        return obj
+
+
+# REGISTER LECTURER VIEW
+class RegisterLecturerView(CreateView):
+    model = User
+    form_class = LecturerRegistrationForm
+    template_name = 'accounts/lecturer/register.html'
+    success_url = '/'
+
+    extra_context = {
+        'title': 'Register'
+    }
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return HttpResponseRedirect(self.get_success_url())
+        return super().dispatch(self.request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+
+        form = self.form_class(data=request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            password = form.cleaned_data.get("password1")
+            user.set_password(password)
+            user.save()
+            return redirect('accounts:login')
+        else:
+            return render(request, 'accounts/lecturer/register.html', {'form': form})
+
+
+# LECTURER PROFILE EDIT VIEW
+class EditLecturerProfileView(UpdateView):
+    model = User
+    form_class = LecturerProfileUpdateForm
+    context_object_name = 'lecturer'
+    template_name = 'accounts/lecturer/edit-profile.html'
+    success_url = reverse_lazy('accounts:lecturer-profile-update')
+
+    @method_decorator(login_required(login_url=reverse_lazy('accounts:login')))
+    @method_decorator(user_is_lecturer)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(self.request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        try:
+            self.object = self.get_object()
+        except Http404:
+            raise Http404("User doesn't exists")
+        # context = self.get_context_data(object=self.object)
+        return self.render_to_response(self.get_context_data())
+
+    def get_object(self, queryset=None):
+        obj = self.request.user
+        print(obj)
+        if obj is None:
+            raise Http404("Lecturer doesn't exists")
+        return obj
+
+
+# LOGIN VIEW FOR BOTH USER
+class LoginView(FormView):
+    success_url = '/'
+    form_class = UserLoginForm
+    template_name = 'accounts/login.html'
+
+    extra_context = {
+        'title': 'Login'
+    }
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return HttpResponseRedirect(self.get_success_url())
+        return super().dispatch(self.request, *args, **kwargs)
+
+    def get_success_url(self):
+        if 'next' in self.request.GET and self.request.GET['next'] != '':
+            return self.request.GET['next']
+        else:
+            return self.success_url
+
+    def get_form_class(self):
+        return self.form_class
+
+    def form_valid(self, form):
+        auth.login(self.request, form.get_user())
+        return HttpResponseRedirect(self.get_success_url())
+
+    def form_invalid(self, form):
+        """If the form is invalid, render the invalid form."""
+        return self.render_to_response(self.get_context_data(form=form))
+
+
+# LOGOUT VIEW
+class LogoutView(RedirectView):
+    url = '/logout'
+
+    def get(self, request, *args, **kwargs):
+        auth.logout(request)
+        messages.success(request, 'You are now logged out')
+        return super(LogoutView, self).get(request, *args, **kwargs)
