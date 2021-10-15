a6957bbef462f9e7cd3d8546fc4ac29339f11261
Akorede Habeebullah <hda0202@gmail.com>
2021-10-15 11:53:29 +0100

    urls

---
+++ accounts/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import *
+
+
+app_name = "accounts"
+
+urlpatterns = [
+    # Students related urls
+    path('student/register', RegisterStudentView.as_view(), name='student-register'),
+    path('student/profile/update/', EditStudentProfileView.as_view(), name='student-profile-update'),
+
+    # Lecturer related urls
+    path('lecturer/register', RegisterLecturerView.as_view(), name='lecturer-register'),
+    path('lecturer/profile/update/', EditLecturerProfileView.as_view(), name='lecturer-profile-update'),
+
+    # login and logout
+    path('login', LoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
+]
