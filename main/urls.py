dd9a92a44ca3185b7ef75593ee87827571ff443c
Akorede Habeebullah <hda0202@gmail.com>
2021-10-18 10:44:00 +0100

    slight edits in settings and urls

--- main/urls.py
+++ main/urls.py
@@ -15,10 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls', namespace='accounts')),
     path('', include('core.urls', namespace='core')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_ROOT, document_root=settings.MEDIA_ROOT)
 
